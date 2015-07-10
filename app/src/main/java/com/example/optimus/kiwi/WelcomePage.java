9eb769a4eced7526a19db043945a9d14eb160bcd
Laurent BRAU <laurent.brau@gmail.com>
2015-07-10 21:39:57 +0200

    commit for test

---
+++ app/src/main/java/com/example/optimus/kiwi/WelcomePage.java
@@ -0,0 +1,107 @@
+package com.example.optimus.kiwi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class WelcomePage extends Activity {
+
+    TextView startGame;
+    TextView addFriend;
+    ListView friendlist;
+    ArrayAdapter<?> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+
+        View activityView = findViewById(R.id.welcome_view);
+
+        startGame = (TextView) activityView.findViewById(R.id.game_btn);
+        addFriend = (TextView) activityView.findViewById(R.id.add_friend_btn);
+        friendlist = (ListView) activityView.findViewById(R.id.friendListView);
+
+        String[] values = new String[]{"Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile"};
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+
+
+        ArrayList<String> userTab = new ArrayList<String>();
+
+        userTab.add("ADLM");
+        userTab.add("Optimus");
+        userTab.add("John");
+        userTab.add("Régis");
+        userTab.add("Adel");
+        userTab.add("Pierre");
+        userTab.add("Adrien");
+        userTab.add("ADLM");
+        userTab.add("Optimus");
+        userTab.add("John");
+        userTab.add("Régis");
+        userTab.add("Adel");
+        userTab.add("Pierre");
+        userTab.add("Adrien");
+
+        ListView h = (ListView) activityView.findViewById(R.id.friendListView);
+        ArrayAdapter adapt = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, userTab);
+
+        //CustomList custom = new CustomList(this, userTab);
+
+        h.setAdapter(adapt);
+
+
+        startGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startGameIntent = new Intent(getBaseContext(), StartGame.class);
+                startActivity(startGameIntent);
+            }
+        });
+
+        addFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent findPlayerIntent = new Intent(getBaseContext(), FindPlayer.class);
+                startActivity(findPlayerIntent);
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.welcome_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
